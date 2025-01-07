33c3b6a8f5cb55ad37f3af5559339b60b0cd8b1a
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-01-07 23:02:23 +0500

    implent logic to execute functions

--- index.js
+++ index.js
@@ -3,23 +3,49 @@ const subtract = (a,b) => (a - b);
 const multiply = (a,b) => (a * b);
 const divide = (a,b) => (a / b);
 
-let numberA = []
-let operator = []
-let numberB = []
+let numberA = 0
+let operator = 0
+let numberB = 0
 
 const operate = (a, operator, b) => (operator(a,b));
 
 let disp = document.getElementById("display");
-let buttonVal = 0
 
 
-document.querySelectorAll('button').forEach((button) => {
+document.querySelectorAll('.number').forEach((button) => {
     button.addEventListener('click', btnAction);
   });
   
   function btnAction(event) {
     const btn = event.target;
-    buttonVal = btn.innerText
     disp.textContent = btn.innerText
+    if (numberA.length > 0) {
+        numberB.push(btn.innerText)
+    } else if(numberB.length > 0) {numberA.push(btn.innerText) }
   }
   
+document.querySelectorAll('.operator').forEach((button) => {
+    button.addEventListener('click', btnAction);
+  });
+  
+  function btnAction(event) {
+    const btn = event.target;
+    disp.textContent = btn.innerText
+    if (btn.innerText === '+') {
+        operator = add
+    }
+    else if (btn.innerText === '-') {
+        operator = subtract
+    }
+    else if (btn.innerText === '*') {
+        operator = multiply
+    }
+    else if (btn.innerText === '/') {
+        operator = divide
+    }
+  }
+
+const equal = document.getElementById('.equal')
+equal.addEventListener("click", () => {
+    disp.textContent = operate(numberA, operator, numberB)
+})
