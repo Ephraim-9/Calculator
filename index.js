f643215ae9e4f36ce63bb5a8518b2c9077591098
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-01-08 13:19:44 +0500

    trial and error

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const subtract = (a,b) => (a - b);
 const multiply = (a,b) => (a * b);
 const divide = (a,b) => (a / b);
 
-let numberA = 0
-let operator = 0
-let numberB = 0
+let numberA = 2
+let operator = add
+let numberB = 3
 
-const operate = (a, operator, b) => (operator(a,b));
+
+const operate = (a, b) => (operator(a,b));
 
 let disp = document.getElementById("display");
-
+let result = 0
 
 document.querySelectorAll('.number').forEach((button) => {
     button.addEventListener('click', btnAction);
@@ -18,10 +19,19 @@ document.querySelectorAll('.number').forEach((button) => {
   
   function btnAction(event) {
     const btn = event.target;
-    disp.textContent = btn.innerText
-    if (numberA.length > 0) {
-        numberB.push(btn.innerText)
-    } else if(numberB.length > 0) {numberA.push(btn.innerText) }
+    for (i = 0; i < 2; i++) {
+        if (i === 0) {
+            numberA = btn.innerText
+            alert(numberA)
+            disp.textContent = btn.innerText
+        }
+        else if (i === 1) {
+            numberB = btn.innerText
+            i = 0
+            alert(numberB)
+            disp.textContent = btn.innerText
+        }
+    }
   }
   
 document.querySelectorAll('.operator').forEach((button) => {
@@ -32,24 +42,26 @@ document.querySelectorAll('.operator').forEach((button) => {
     const btn = event.target;
     disp.textContent = btn.innerText
     if (btn.innerText === '+') {
-        operator = "add"
+        let operator = add
         alert(operator)
     }
     else if (btn.innerText === '-') {
-        operator = "subtract"
+        let operator = subtract
         alert(operator)
     }
     else if (btn.innerText === '*') {
-        operator = "multiply"
+        let operator = multiply
         alert(operator)
     }
     else if (btn.innerText === '/') {
-        operator = "divide"
+        let operator = divide
         alert(operator)
     }  
   }
 
 const equal = document.querySelector('#equal')
 equal.addEventListener("click", () => {
-    disp.textContent = operate(numberA, operator, numberB)
+    result = operate(numberA, numberB)
+    disp.textContent = result
+    alert(result)
 })
